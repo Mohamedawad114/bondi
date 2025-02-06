ff224846be1cea066c92f3af1c4ba940001e46ef
Mohamed Awad <86704934+Mohamedawad114@users.noreply.github.com>
2025-02-06 15:33:41 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ let btn = document.getElementById('btn');
 btn.onclick = function(){
     window.scrollTo(0,0);
 }
+window.onscroll = function () {
+    if (window.scrollY <= 200) {
+        btn.style.display = "none";
+    } else {
+        btn.style.display = "block";
+    }
+}
